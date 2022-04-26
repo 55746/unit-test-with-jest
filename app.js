e47b44b36eda05579ca2938d4bfd8faac3654bc5
55746 <Lawhead.gavin@live.jobcorps.org>
2022-04-26 18:45:51 +0000

    updated

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const sum = (a, b) => {
 console.log(sum(7, 3));
 
 const fromEuroToDollar = function (valueInEuro) {
-  let valueInDollar = valueInEuro * 0.94;
+  let valueInDollar = valueInEuro * 1.2;
   return valueInDollar;
 };
 const fromDollarToYen = function (valueInDollar) {
-  let valueInYen = valueInDollar * 127.38;
+  let valueInYen = (valueInDollar * 127.9) / 1.2;
   return valueInYen;
 };
 const fromYenToPound = function (valueInYen) {
-  let valueInPound = valueInYen * 0.0062;
+  let valueInPound = (valueInYen * 0.8) / 127.9;
   return valueInPound;
 };
 
--- test.js
+++ test.js
@@ -5,22 +5,22 @@ test("adds 14 + 9 to equal 23,", () => {
   expect(total).toBe(23);
 });
 
-test("10 Euro should be 9.40 Dollars", function () {
+test("10 Euro should be 12 Dollars", function () {
   //import the function from app.js
   const { fromEuroToDollar } = require("./app.js");
   let total = fromEuroToDollar(10);
-  let expected = 10 * 0.94;
+  let expected = 10 * 1.2;
   expect(total).toBe(expected); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
 });
-test("10 Dollars should be 1273.80 Yen", function () {
+test("10 Dollars should be .09 Yen", function () {
   const { fromDollarToYen } = require("./app.js");
   let total = fromDollarToYen(10);
-  let expected = 10 * 127.38;
+  let expected = (10 * 127.9) / 1.2;
   expect(total).toBe(expected);
 });
-test("10 Yen should be .062 Pounds, or less than one Pence", function () {
+test("10 Yen should be .06 Pounds, or less than one Pence", function () {
   const { fromYenToPound } = require("./app.js");
   let total = fromYenToPound(10);
-  let expected = 10 * 0.0062;
+  let expected = (10 * 0.8) / 127.9;
   expect(total).toBe(expected);
 });
